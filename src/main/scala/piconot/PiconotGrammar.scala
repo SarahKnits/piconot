91146ddb47be335fdb19e0cea9e601f39cf60d60
Hayden Blauzvern <hblauzvern@gmail.com>
2014-09-27 20:18:10 -0700

    Added the translation from our language to picobot language

--- src/main/scala/piconot/PiconotGrammar.scala
+++ src/main/scala/piconot/PiconotGrammar.scala
@@ -1,112 +1,159 @@
 package piconot
 
+import picolib.semantics._
 
 object PiconotGrammar extends App {
 
-  def If = {
-    You
-  }
+  var rules: List[Rule] = List()
+  var streetsToStates: Map[String, String] = Map()
 
-  If. you. are. on("Second")("St."). and. can. go. uptown. and. cannot. go. outta_town. then. teleport. to("First")("St.")
-  If. you. are. on("Main")("St."). and. cannot. go. into_town. and. can. go. downtown. then. go. downtown. on("Second")("Blvd.")
-}
+  If.you.are.on("Second")("St.").and.can.go.uptown.and.cannot.go.outta_town.then.teleport.to("First")("St.")
+  If.you.are.on("Main")("St.").and.cannot.go.into_town.and.can.go.downtown.then.go.downtown.on("Second")("St.")
+  rules.foreach(x => println(x))
 
-object You {
-  def you = {
-    Are
-  }
-}
-
-object Are {
-  def are = {
-   On
-  }
-}
-
-object On {
-  def on(name:String)(modifier:String):And = {
-    val open = Array.fill[Int](4)(-1)
-    new And(name, modifier, open)
-  }
-}
-
-class And(name:String, modifier:String, open:Array[Int]) {
-  def and:Can = {
-    new Can(name, modifier, open)
-  }
-  def then:GoNext = {
-    new GoNext(name, modifier, open)
-  }
-}
-
-class GoNext(name:String, modifier:String, open:Array[Int]) {
-  def go:DirectionNext = {
-    new DirectionNext(name, modifier, open)
-  }
-  def teleport:OnNext = {
-    new OnNext(name, modifier, open, -1)
-  }
-}
-
-class Can(name:String, modifier:String, open:Array[Int]) {
-  def can:Go = {
-    new Go(name, modifier, open, true)
-  }
-  def cannot:Go = {
-    new Go(name, modifier, open, false)
-  }
-}
-
-class Go(name:String, modifier:String, open:Array[Int], can:Boolean) {
-  def go:Direction = {
-    new Direction(name, modifier, open, can)
-  }
-}
-
-class Direction(name:String, modifier:String, open:Array[Int], can:Boolean) {
-  def uptown:And = {
-    if (can) open(0) = 0 else open(0) = 1
-    new And(name, modifier, open)
-  }
-  def outta_town:And = {
-    if (can) open(1) = 0 else open(1) = 1
-    new And(name, modifier, open)
-  }
-  def into_town:And = {
-    if (can) open(2) = 0 else open(2) = 1
-    new And(name, modifier, open)
-  }
-  def downtown:And = {
-    if (can) open(3) = 0 else open(3) = 1
-    new And(name, modifier, open)
-  }
-
-}
-
-class DirectionNext(name:String, modifier:String, open:Array[Int]) {
-  def uptown:OnNext = {
-    new OnNext(name, modifier, open,0)
-  }
-  def outta_town:OnNext = {
-    new OnNext(name, modifier, open, 1)
-  }
-  def into_town:OnNext = {
-    new OnNext(name, modifier, open, 2)
-  }
-  def downtown:OnNext = {
-    new OnNext(name, modifier, open, 3)
-  }
-}
-
-class OnNext(name:String, modifier:String, open:Array[Int], finalDirection:Int) {
-  def on(name2:String)(modifier2:String) = {
-    for (x <- Range(0,4)) {
-      println(open(x))
+  object If {
+    def you = {
+      Are
     }
   }
-  def to(name2:String)(modifier2:String) = {
-    for (x <- Range(0,4)) {
-      println(open(x))
+
+  object Are {
+    def are = {
+      On
+    }
+  }
+
+  object On {
+    def on(name: String)(modifier: String): And = {
+      val open = Array.fill[Int](4)(-1)
+      new And(name, modifier, open)
+    }
+  }
+
+  class And(name: String, modifier: String, open: Array[Int]) {
+    def and: Can = {
+      new Can(name, modifier, open)
+    }
+
+    def then: GoNext = {
+      new GoNext(name, modifier, open)
+    }
+  }
+
+  class GoNext(name: String, modifier: String, open: Array[Int]) {
+    def go: DirectionNext = {
+      new DirectionNext(name, modifier, open)
+    }
+
+    def teleport: OnNext = {
+      new OnNext(name, modifier, open, -1)
+    }
+  }
+
+  class Can(name: String, modifier: String, open: Array[Int]) {
+    def can: Go = {
+      new Go(name, modifier, open, true)
+    }
+
+    def cannot: Go = {
+      new Go(name, modifier, open, false)
+    }
+  }
+
+  class Go(name: String, modifier: String, open: Array[Int], can: Boolean) {
+    def go: Direction = {
+      new Direction(name, modifier, open, can)
+    }
+  }
+
+  class Direction(name: String, modifier: String, open: Array[Int], can: Boolean) {
+    def uptown: And = {
+      if (can) open(0) = 0 else open(0) = 1
+      new And(name, modifier, open)
+    }
+
+    def outta_town: And = {
+      if (can) open(1) = 0 else open(1) = 1
+      new And(name, modifier, open)
+    }
+
+    def into_town: And = {
+      if (can) open(2) = 0 else open(2) = 1
+      new And(name, modifier, open)
+    }
+
+    def downtown: And = {
+      if (can) open(3) = 0 else open(3) = 1
+      new And(name, modifier, open)
+    }
+
+  }
+
+  class DirectionNext(name: String, modifier: String, open: Array[Int]) {
+    def uptown: OnNext = {
+      new OnNext(name, modifier, open, 0)
+    }
+
+    def outta_town: OnNext = {
+      new OnNext(name, modifier, open, 1)
+    }
+
+    def into_town: OnNext = {
+      new OnNext(name, modifier, open, 2)
+    }
+
+    def downtown: OnNext = {
+      new OnNext(name, modifier, open, 3)
+    }
+  }
+
+  class OnNext(name: String, modifier: String, open: Array[Int], finalDirection: Int) {
+    def on(nameNext: String)(modifierNext: String) = {
+      rules = rules :+ Rule(getState(name + " " + modifier),
+        getSurroundings(open),
+        getDirection(finalDirection),
+        getState(nameNext + " " + modifierNext))
+    }
+
+    def to(nameNext: String)(modifierNext: String) = {
+      rules = rules :+ Rule(getState(name + " " + modifier),
+        getSurroundings(open),
+        getDirection(finalDirection),
+        getState(nameNext + " " + modifierNext))
+    }
+  }
+
+  //----------------------------------------
+  def getState(street: String): State = {
+    streetsToStates.get(street) match {
+      case Some(stateNum:String) => State(stateNum)
+      case None => {
+        streetsToStates = streetsToStates + (street -> streetsToStates.size.toString) // not very scala-y
+        getState(street)
+      }
+    }
+  }
+
+  def getRelDes(action: Int): RelativeDescription = {
+    action match {
+      case 0 => Open // can
+      case 1 => Blocked // can't
+      case -1 => Anything // default
+    }
+  }
+
+  def getSurroundings(surroundings: Array[Int]): Surroundings = {
+    Surroundings(getRelDes(surroundings(0)), getRelDes(surroundings(1)),
+      getRelDes(surroundings(2)), getRelDes(surroundings(3)))
+  }
+
+  def getDirection(direction: Int): MoveDirection = {
+    direction match {
+      case 0 => North
+      case 1 => East
+      case 2 => West
+      case 3 => South
+      case _ => StayHere
     }
   }
 }
